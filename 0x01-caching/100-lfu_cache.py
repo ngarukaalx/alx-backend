e060ec0c703ce4ea79e769e6c2e066720df87bd8
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-03 15:01:23 +0000

    lfu

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""LFUCache module"""
+from base_caching import BaseCaching
+import time
+
+
+class LFUCache(BaseCaching):
+    """class implemention LFU"""
+    def __init__(self):
+        """class constructor"""
+        self.timestamp = {}
+        super().__init__()
+
+    def put(self, key, item):
+        """implements LFU alogarithm"""
+        if key is None or item is None:
+            return
+        # check if key exists
+        exists_key = self.cache_data.get(key)
+        if exists_key:
+            # update it in cache_data dict
+            self.cache_data[key] = item
+            # update tracker dict in timestamp using same key
+            self.timestamp[key] += 1
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # get the list time in tracker dict timestamp
+            list_used = min(self.timestamp, key=self.timestamp.get)
+            # delete the key from cache_data
+            del self.cache_data[list_used]
+            # delete also from tracker dict
+            del self.timestamp[list_used]
+            print("DISCARD: {}".format(list_used))
+        # add key in both cache_data and tracker
+        self.cache_data[key] = item
+        self.timestamp[key] = time.time()
+
+    def get(self, key):
+        """retrive a value given a key"""
+        if key is None:
+            return None
+        # get return val or else None
+        val = self.cache_data.get(key)
+        if val:
+            self.timestamp[key] += 1
+        return val
