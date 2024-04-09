1cb28645ea1cd78381f237d80abbc7322532a298
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-09 17:02:35 +0000

    babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+from flask import Flask, render_template
+from typing import Any
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """list of available languages"""
+    LANGUAGES = ['en', 'fr']
+
+# use Config to set Babel's default locale "en"
+# and timezones ("UTC")
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+babel = Babel(app)
+
+@app.route('/', strict_slashes=False)
+def hello_ba() -> Any:
+    """Display an html page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    """Main func"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
