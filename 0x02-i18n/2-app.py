ceeef789b5026981504138701c4b88746f7dc971
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-09 20:51:12 +0000

    get_locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+from flask import Flask, render_template, request
+from typing import Any, Optional
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """list of available languages"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# use Config to set Babel's default locale "en"
+# and timezones ("UTC")
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """determine the best match of lang"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/', strict_slashes=False)
+def hello_ba() -> Any:
+    """Display an html page"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    """Main func"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
